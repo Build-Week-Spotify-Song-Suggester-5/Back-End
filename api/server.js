821f30ec66f4fcd37a88e719ae0eeed1588b868d
Hiter Harris <hiterharris@comcast.net>
2020-03-01 14:09:58 -0500

    added restricted middlewate to users endpoint, logout

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
+const restricted = require('../auth/restricted-middleware');
 
 const server = express();
 
@@ -12,11 +13,10 @@ server.use(express.json());
 server.use(cors());
 
 server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
-
+server.use('/api/users', restricted, usersRouter);
 
 server.get('/', (req, res) => {
     res.send("Server endpoint");
   });
 
-  module.exports = server;
+module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-model.js');
+const {jwtSecret} = require('../config/secrets');
 
 router.post('/register', (req, res) => {
   let user = req.body;
@@ -10,7 +12,6 @@ router.post('/register', (req, res) => {
 
   Users.add(user)
     .then(saved => {
-      req.session.loggedIn = true;
       res.status(201).json(saved);
     })
     .catch(error => {
@@ -25,10 +26,10 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        req.session.loggedIn = true;
-        req.session.username = user.username;
+        const token = generateToken(user);
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token,
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -52,5 +53,17 @@ router.get('/logout', (req, res) => {
     res.status(200).json({message: 'Goodbye!'})
   }
 })
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    role: user.role || 'user'
+  };
+  const options = {
+    expiresIn: '1h',
+  }
+  return jwt.sign(payload, jwtSecret, options);
+}
 
 module.exports = router;
